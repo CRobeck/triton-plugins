003c7ba5eff08d8520290c14973c4fa6de5991aa
Corbin Robeck <robeck@meta.com>
2025-09-21 14:13:57 -0400

    clean up

--- build_script.sh
+++ build_script.sh
@@ -1,3 +1,7 @@
-mkdir build && cd build
-cmake -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DLLVM_INSTALL_DIR=/Users/corbin/.triton/llvm/llvm-064f02da-macos-arm64/ ..
-cmake --build .
+cmake -DCMAKE_C_COMPILER=clang \
+-DCMAKE_CXX_COMPILER=clang++ \
+-DLLVM_INSTALL_DIR=/Users/corbin/.triton/llvm/llvm-064f02da-macos-arm64/ \
+-DTRITON_BASE_DIR=/Users/corbin/Desktop/triton/ \
+-DTRITON_INSTALL_DIR=/Users/corbin/Desktop/triton/build/cmake.macosx-15.4-arm64-cpython-3.13/ \
+-DTRITON_LIB_DIR=/Users/corbin/Desktop/triton/python/triton/_C/ \
+..

0d59c3f1caaef6f32b4e43787cfb3ab1ba645f1f
Corbin Robeck <robeck@meta.com>
2025-09-23 11:27:32 -0700

    update

--- build_script.sh
+++ build_script.sh
@@ -1,7 +1,10 @@
+#export LLVM_BUILD_DIR=\`pwd\`/llvm-project/build
+#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/.conda/envs/triton/lib
+
 cmake -DCMAKE_C_COMPILER=clang \
 -DCMAKE_CXX_COMPILER=clang++ \
--DLLVM_INSTALL_DIR=/Users/corbin/.triton/llvm/llvm-064f02da-macos-arm64/ \
--DTRITON_BASE_DIR=/Users/corbin/Desktop/triton/ \
--DTRITON_INSTALL_DIR=/Users/corbin/Desktop/triton/build/cmake.macosx-15.4-arm64-cpython-3.13/ \
--DTRITON_LIB_DIR=/Users/corbin/Desktop/triton/python/triton/_C/ \
+-DLLVM_INSTALL_DIR=/home/robeck/.triton/llvm/llvm-064f02da-ubuntu-x64 \
+-DTRITON_BASE_DIR=/home/robeck/triton \
+-DTRITON_INSTALL_DIR=/home/robeck/triton/build/cmake.linux-x86_64-cpython-3.11 \
+-DTRITON_LIB_DIR=/home/robeck/triton/python/triton/_C/ \
 ..
--- lib/ExtensionHello.cpp
+++ lib/ExtensionHello.cpp
@@ -1,60 +1,49 @@
-#include "mlir/Analysis/SliceAnalysis.h"
-#include "mlir/Dialect/SCF/IR/SCF.h"
-#include "mlir/IR/BuiltinAttributes.h"
-#include "mlir/IR/Dominance.h"
-#include "mlir/IR/IRMapping.h"
-#include "mlir/IR/Matchers.h"
-#include "mlir/IR/PatternMatch.h"
-#include "mlir/IR/Verifier.h"
-#include "mlir/Interfaces/InferTypeOpInterface.h"
-#include "mlir/Interfaces/SideEffectInterfaces.h"
+// #include "mlir/Analysis/SliceAnalysis.h"
+// #include "mlir/Dialect/SCF/IR/SCF.h"
+// #include "mlir/IR/BuiltinAttributes.h"
+// #include "mlir/IR/Dominance.h"
+// #include "mlir/IR/IRMapping.h"
+// #include "mlir/IR/Matchers.h"
+// #include "mlir/IR/PatternMatch.h"
+// #include "mlir/IR/Verifier.h"
+// #include "mlir/Interfaces/InferTypeOpInterface.h"
+// #include "mlir/Interfaces/SideEffectInterfaces.h"
 #include "mlir/Pass/Pass.h"
 #include "mlir/Pass/PassManager.h"
-#include "mlir/Support/LogicalResult.h"
-#include "mlir/Transforms/GreedyPatternRewriteDriver.h"
-#include "mlir/Transforms/Passes.h"
-#include "mlir/Transforms/RegionUtils.h"
+// #include "mlir/Support/LogicalResult.h"
+// #include "mlir/Transforms/GreedyPatternRewriteDriver.h"
+// #include "mlir/Transforms/Passes.h"
+// #include "mlir/Transforms/RegionUtils.h"
 #include "triton/Analysis/Utility.h"
-#include "triton/Dialect/Triton/IR/Dialect.h"
-#include "triton/Dialect/TritonGPU/IR/Dialect.h"
-#include "triton/Dialect/TritonGPU/Transforms/Passes.h"
-#include "triton/Dialect/TritonGPU/Transforms/TritonGPUConversion.h"
-#include "triton/Dialect/TritonGPU/Transforms/Utility.h"
-#include <deque>
+// #include "triton/Dialect/Triton/IR/Dialect.h"
+// #include "triton/Dialect/TritonGPU/IR/Dialect.h"
+// #include "triton/Dialect/TritonGPU/Transforms/Passes.h"
+// #include "triton/Dialect/TritonGPU/Transforms/TritonGPUConversion.h"
+// #include "triton/Dialect/TritonGPU/Transforms/Utility.h"
+// #include <deque>
 
-#include "mlir/IR/PatternMatch.h"
-#include "mlir/Transforms/GreedyPatternRewriteDriver.h"
-#include "triton/Dialect/TritonGPU/Transforms/Passes.h"
+// #include "mlir/IR/PatternMatch.h"
+// #include "mlir/Transforms/GreedyPatternRewriteDriver.h"
+// #include "triton/Dialect/TritonGPU/Transforms/Passes.h"
 
-#include "mlir/IR/MLIRContext.h"
-#include "mlir/InitAllDialects.h"
-#include "mlir/Tools/Plugins/DialectPlugin.h"
+// #include "mlir/IR/MLIRContext.h"
+// #include "mlir/InitAllDialects.h"
+// #include "mlir/Tools/Plugins/DialectPlugin.h"
 
 #include "mlir/Tools/Plugins/PassPlugin.h"
-#include "llvm/Config/llvm-config.h"
-#include "llvm/Support/Compiler.h"
-#include "mlir/Support/TypeID.h"
+// #include "llvm/Config/llvm-config.h"
+// #include "llvm/Support/Compiler.h"
+// #include "mlir/Support/TypeID.h"
 
 using namespace mlir;
 
-/// Dialect plugin registration mechanism.
-/// Observe that it also allows to register passes.
-/// Necessary symbol to register the dialect plugin.
-// extern "C" LLVM_ATTRIBUTE_WEAK DialectPluginLibraryInfo
-// mlirGetDialectPluginInfo() {
-//   return {MLIR_PLUGIN_API_VERSION, "Standalone", LLVM_VERSION_STRING,
-//           [](DialectRegistry *registry) {
-//             registry->insert<mlir::standalone::StandaloneDialect>();
-//             mlir::standalone::registerPasses();
-//           }};
-// }
 
-#include "mlir/Pass/Pass.h"
-#include "mlir/IR/PatternMatch.h"
-#include "mlir/Transforms/GreedyPatternRewriteDriver.h"
-#include "triton/Dialect/TritonGPU/Transforms/Passes.h"
+// #include "mlir/Pass/Pass.h"
+// #include "mlir/IR/PatternMatch.h"
+// #include "mlir/Transforms/GreedyPatternRewriteDriver.h"
+// #include "triton/Dialect/TritonGPU/Transforms/Passes.h"
 
-extern "C" void FOOBAR() { }
+// extern "C" void FOOBAR() { }
 
 namespace mlir {
 namespace triton {
